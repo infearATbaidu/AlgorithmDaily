588926e43ed6f4efa8bb7673fbff3c0456a179bb
zhanggang02 <zhanggang02@baidu.com>
2017-06-11 14:59:24 +0800

    intersection-of-two-arrays

--- src/leetcode/Solution.java
+++ src/leetcode/Solution.java
@@ -1,8 +1,6 @@
 package leetcode;
 
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Stack;
+import java.util.*;
 
 /**
  * Created by infear on 2017/6/11.
@@ -45,5 +43,29 @@ public class Solution {
         }
         return r;
 
+    }
+
+    // https://leetcode.com/problems/intersection-of-two-arrays/#/description
+    public int[] intersection(int[] nums1, int[] nums2) {
+        if (nums1 == null || nums2 == null) {
+            return null;
+        }
+        Set s2 = new HashSet();
+        for (int ele : nums2) {
+            s2.add(ele);
+        }
+
+        Set<Integer> r = new HashSet<>();
+        for (int ele : nums1) {
+            if (s2.contains(ele)) {
+                r.add(ele);
+            }
+        }
+        int result[] = new int[r.size()];
+        int i = 0;
+        for (int ele : r) {
+            result[i++] = ele;
+        }
+        return result;
     }
 }
