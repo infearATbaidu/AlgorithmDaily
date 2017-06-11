de4211218daf21ff488e21ee206b42abc2a76d60
zhanggang02 <zhanggang02@baidu.com>
2017-06-11 15:28:04 +0800

    find-the-difference

--- src/leetcode/Solution.java
+++ src/leetcode/Solution.java
@@ -68,4 +68,25 @@ public class Solution {
         }
         return result;
     }
+
+    // https://leetcode.com/problems/find-the-difference/#/description
+    public char findTheDifference(String s, String t) {
+        char[] ss = s.toCharArray(), tt = t.toCharArray();
+        Map<Character, Integer> si = new HashMap<>();
+        Map<Character, Integer> ti = new HashMap<>();
+        for (char c : ss) {
+            int num = si.getOrDefault(c, 0);
+            si.put(c, num + 1);
+        }
+        for (char c : tt) {
+            int num = ti.getOrDefault(c, 0);
+            ti.put(c, num + 1);
+        }
+        for (Character c : ti.keySet()) {
+            if (!si.getOrDefault(c, 0).equals(ti.getOrDefault(c, 0))) {
+                return c;
+            }
+        }
+        return 'p';
+    }
 }
