ffaedd2d69a845367555a742f85f969277241462
zhanggang02 <zhanggang02@baidu.com>
2020-01-09 10:03:24 +0800

    Permutations

---
+++ src/leetcode/Permutations.java
@@ -0,0 +1,35 @@
+package leetcode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * https://leetcode.com/problems/permutations/submissions/
+ *
+ * @author infear
+ */
+public class Permutations {
+    public List<List<Integer>> permute(int[] nums) {
+        List<List<Integer>> r = new ArrayList<>();
+        r.add(new ArrayList<>());
+
+        int i = 0;
+        while (i != nums.length) {
+            int ll = r.size(), j = 0;
+            while (j != ll) {
+                int k = 0;
+                while (k != r.get(j).size()) {
+                    List<Integer> copy = new ArrayList<>();
+                    copy.addAll(r.get(j));
+                    copy.add(k, nums[i]);
+                    k++;
+                    r.add(copy);
+                }
+                r.get(j).add(nums[i]);
+                j++;
+            }
+            i++;
+        }
+        return r;
+    }
+}
