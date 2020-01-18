e39c8f0aa1042a928700ac2acba74ad8797b1634
zhanggang02 <zhanggang02@baidu.com>
2020-01-18 10:19:55 +0800

    insertIntoBST

---
+++ go/insertIntoBST.go
@@ -0,0 +1,26 @@
+package _go
+
+// https://leetcode.com/problems/insert-into-a-binary-search-tree/
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func insertIntoBST(root *TreeNode, val int) *TreeNode {
+	if root == nil {
+		var newroot TreeNode
+		newroot.Val = val
+		return &newroot
+	}
+	if root.Val > val {
+		root.Left = insertIntoBST(root.Left, val)
+		return root
+	}
+	if root.Val < val {
+		root.Right = insertIntoBST(root.Right, val)
+		return root
+	}
+	return nil
+}
