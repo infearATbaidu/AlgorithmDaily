1abca1aaab43e2ab0590760e2df78f92b1af05f0
zhanggang02 <zhanggang02@baidu.com>
2020-03-15 15:13:35 +0800

    spring WebfluxDemoApplication

---
+++ spring-way/webflux-demo/src/main/java/com/getset/webfluxdemo/controller/UserController.java
@@ -0,0 +1,43 @@
+package com.getset.webfluxdemo.controller;
+
+import com.getset.webfluxdemo.model.User;
+import com.getset.webfluxdemo.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+import java.time.Duration;
+
+@RestController
+@RequestMapping("/user")
+public class UserController {
+    @Autowired
+    private UserService userService;
+
+    @PostMapping("")
+    public Mono<User> save(User user) {
+        return this.userService.save(user);
+    }
+
+    @DeleteMapping("/{username}")
+    public Mono<Long> deleteByUsername(@PathVariable String username) {
+        return this.userService.deleteByUsername(username);
+    }
+
+    @GetMapping("/{username}")
+    public Mono<User> findByUsername(@PathVariable String username) {
+        return this.userService.findByUsername(username);
+    }
+
+    @GetMapping(value = "", produces = MediaType.APPLICATION_STREAM_JSON_VALUE)
+    public Flux<User> findAll() {
+        return this.userService.findAll().delayElements(Duration.ofSeconds(1));
+    }
+}
