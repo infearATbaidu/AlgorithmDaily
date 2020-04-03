ebec2d9bee5ca376b98ac1a78093f9e369533587
zhanggang02 <zhanggang02@baidu.com>
2020-04-03 21:38:24 +0800

    2020-04-03 List

--- algorithm/java/likou/top/interview/questions/ListSolution.java
+++ algorithm/java/likou/top/interview/questions/ListSolution.java
@@ -1,5 +1,7 @@
 package likou.top.interview.questions;
 
+import java.util.HashMap;
+
 /**
  * @author infear
  */
@@ -44,12 +46,124 @@ public class ListSolution {
         return true;
     }
 
+    public Node copyRandomList(Node head) {
+        if (head == null) {
+            return null;
+        }
+        Node vhead = new Node(-1), pre = vhead, cur = head;
+        HashMap<Node, Node> m = new HashMap<>();
+        while (cur != null) {
+            Node copy = new Node(cur.val);
+            copy.random = cur;
+            m.put(cur, copy);
+            cur = cur.next;
+            pre.next = copy;
+            pre = copy;
+        }
+        cur = vhead.next;
+        while (cur != null) {
+            if (cur.random.random != null) {
+                cur.random = m.get(cur.random.random);
+            } else {
+                cur.random = null;
+            }
+            cur = cur.next;
+        }
+        return vhead.next;
+    }
+
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        ListNode cur = headA;
+        int lengthA = 0;
+        while (cur != null) {
+            cur = cur.next;
+            lengthA++;
+        }
+        cur = headB;
+        int lengthB = 0;
+        while (cur != null) {
+            cur = cur.next;
+            lengthB++;
+        }
+        ListNode cur1, cur2;
+        if (lengthA >= lengthB) {
+            cur = headA;
+            int gap = lengthA - lengthB;
+            while (gap != 0) {
+                cur = cur.next;
+                gap--;
+            }
+            cur1 = cur;
+            cur2 = headB;
+        } else {
+            cur = headB;
+            int gap = lengthB - lengthA;
+            while (gap != 0) {
+                cur = cur.next;
+                gap--;
+            }
+            cur1 = headA;
+            cur2 = cur;
+        }
+        while (cur1 != null && cur2 != null && cur1 != cur2) {
+            cur1 = cur1.next;
+            cur2 = cur2.next;
+        }
+        if (cur1 == null) {
+            return null;
+        }
+        return cur1;
+    }
+
+    public void deleteNode(ListNode node) {
+        ListNode cur = node;
+        while (cur.next != null) {
+            cur.val = cur.next.val;
+            if (cur.next.next == null) {
+                cur.next = null;
+                break;
+            }
+            cur = cur.next;
+        }
+    }
+
+    public boolean hasCycle(ListNode head) {
+        ListNode slow = head, fast = slow;
+        while (true) {
+            if (fast != null && fast.next != null) {
+                fast = fast.next.next;
+            } else {
+                return false;
+            }
+            if (slow != null) {
+                slow = slow.next;
+            } else {
+                return false;
+            }
+            if (slow == fast) {
+                return true;
+            }
+        }
+    }
+
     class ListNode {
         int val;
         ListNode next;
 
         ListNode(int x) {
             val = x;
+        }
+    }
+
+    class Node {
+        int val;
+        Node next;
+        Node random;
+
+        public Node(int val) {
+            this.val = val;
+            this.next = null;
+            this.random = null;
         }
     }
 }
