13fe142b2e95d0cb6fb7082aad12ef11e798d944
zhanggang02 <zhanggang02@baidu.com>
2020-04-18 16:23:17 +0800

    RandomizedSet

--- algorithm/java/likou/top/interview/questions/SortAndIndexSolution.java
+++ algorithm/java/likou/top/interview/questions/SortAndIndexSolution.java
@@ -9,7 +9,7 @@ import java.util.stream.Collectors;
  */
 public class SortAndIndexSolution {
     public static void main(String args[]) {
-        new SortAndIndexSolution().largestNumber(new int[] {10, 2});
+        new SortAndIndexSolution().wiggleSort(new int[] {1, 5, 1, 1, 6, 4});
     }
 
     /*    最大数
@@ -123,5 +123,47 @@ public class SortAndIndexSolution {
                 e = mid;
             }
         }
+    }
+
+    /*    摆动排序 II
+        给定一个无序的数组 nums，将它重新排列成 nums[0] < nums[1] > nums[2] < nums[3]... 的顺序。
+
+        示例 1:
+
+        输入: nums = [1, 5, 1, 1, 6, 4]
+        输出: 一个可能的答案是 [1, 4, 1, 5, 1, 6]
+        示例 2:
+
+        输入: nums = [1, 3, 2, 2, 3, 1]
+        输出: 一个可能的答案是 [2, 3, 1, 3, 1, 2]
+        说明:
+        你可以假设所有输入都会得到有效的结果。
+
+        进阶:
+        你能用 O(n) 时间复杂度和 / 或原地 O(1) 额外空间来实现吗？*/
+    public void wiggleSort(int[] nums) {
+        Arrays.sort(nums);
+        int copy[] = new int[nums.length];
+        int j = nums.length / 2;
+        int k = nums.length - 1;
+        if (nums.length % 2 == 0) {
+            j = nums.length / 2 - 1;
+        }
+        for (int i = 0; i != nums.length; i++) {
+            if (i % 2 == 0) {
+                copy[i] = nums[j--];
+            } else {
+                copy[i] = nums[k--];
+            }
+        }
+        for (int i = 0; i != nums.length; i++) {
+            nums[i] = copy[i];
+        }
+    }
+
+    private void swap(int[] nums, int i, int j) {
+        int tmp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = tmp;
     }
 }
