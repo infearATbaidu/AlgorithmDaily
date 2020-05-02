57bf29589478b66c6416a97409d7a2d7ef2028fd
zhanggang02 <zhanggang02@baidu.com>
2020-05-02 11:26:25 +0800

    2020.05.02 countSmaller

--- algorithm/java/likou/top/interview/questions/SortAndIndexSolution.java
+++ algorithm/java/likou/top/interview/questions/SortAndIndexSolution.java
@@ -1,6 +1,7 @@
 package likou.top.interview.questions;
 
 import java.util.Arrays;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -165,5 +166,40 @@ public class SortAndIndexSolution {
         int tmp = nums[i];
         nums[i] = nums[j];
         nums[j] = tmp;
+    }
+
+    /*    计算右侧小于当前元素的个数
+        给定一个整数数组 nums，按要求返回一个新数组 counts。数组 counts 有该性质： counts[i] 的值是  nums[i] 右侧小于 nums[i] 的元素的数量。
+
+        示例:
+
+        输入: [5,2,6,1]
+        输出: [2,1,1,0]
+        解释:
+                5 的右侧有 2 个更小的元素 (2 和 1).
+                2 的右侧仅有 1 个更小的元素 (1).
+                6 的右侧有 1 个更小的元素 (1).
+                1 的右侧有 0 个更小的元素.*/
+    public List<Integer> countSmaller(int[] nums) {
+        List<Integer> r = new LinkedList<>();
+        if (nums.length == 0) {
+            return r;
+        }
+        r.add(0, 0);
+        for (int i = nums.length - 2; i != -1; i--) {
+            int ele = nums[i];
+            int k = i + 1;
+            while (k != nums.length) {
+                if (nums[k] < ele) {
+                    nums[k - 1] = nums[k];
+                    k++;
+                } else {
+                    break;
+                }
+            }
+            nums[k - 1] = ele;
+            r.add(0, k - i - 1);
+        }
+        return r;
     }
 }
