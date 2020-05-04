4dcb929c67804ad123a2c93c6feb5462759173d6
zhanggang02 <zhanggang02@baidu.com>
2020-05-04 16:46:56 +0800

    canJump

--- algorithm/java/likou/top/interview/questions/DpSolution.java
+++ algorithm/java/likou/top/interview/questions/DpSolution.java
@@ -342,4 +342,40 @@ public class DpSolution {
         }
         return arr[m - 1][n - 1];
     }
+
+
+/*    跳跃游戏
+    给定一个非负整数数组，你最初位于数组的第一个位置。
+
+    数组中的每个元素代表你在该位置可以跳跃的最大长度。
+
+    判断你是否能够到达最后一个位置。
+
+    示例 1:
+
+    输入: [2,3,1,1,4]
+    输出: true
+    解释: 我们可以先跳 1 步，从位置 0 到达 位置 1, 然后再从位置 1 跳 3 步到达最后一个位置。
+    示例 2:
+
+    输入: [3,2,1,0,4]
+    输出: false
+    解释: 无论怎样，你总会到达索引为 3 的位置。但该位置的最大跳跃长度是 0 ， 所以你永远不可能到达最后一个位置。*/
+
+    public boolean canJump(int[] nums) {
+        if (nums.length == 0) {
+            return false;
+        }
+        boolean r[] = new boolean[nums.length];
+        r[nums.length - 1] = true;
+        for (int i = nums.length - 2; i != -1; i--) {
+            for (int j = 1; j <= nums[i]; j++) {
+                if (i + j >= nums.length - 1 || r[i + j]) {
+                    r[i] = true;
+                    break;
+                }
+            }
+        }
+        return r[0];
+    }
 }
