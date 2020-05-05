993c02a516f6645710ee45c7a1635c419519e45e
zhanggang02 <zhanggang02@baidu.com>
2020-05-05 14:06:58 +0800

    checkInclusion

--- algorithm/java/likou/top/interview/questions/StringSolution.java
+++ algorithm/java/likou/top/interview/questions/StringSolution.java
@@ -706,6 +706,45 @@ public class StringSolution {
         return r;
     }
 
+    /*    字符串的排列
+        给定两个字符串 s1 和 s2，写一个函数来判断 s2 是否包含 s1 的排列。
+        换句话说，第一个字符串的排列之一是第二个字符串的子串。
+        示例1:
+        输入: s1 = "ab" s2 = "eidbaooo"
+        输出: True
+        解释: s2 包含 s1 的排列之一 ("ba").
+
+        示例2:
+        输入: s1= "ab" s2 = "eidboaoo"
+        输出: False
+        注意：
+
+        输入的字符串只包含小写字母
+        两个字符串的长度都在 [1, 10,000] 之间*/
+    public boolean checkInclusion(String s1, String s2) {
+        int[] vals = new int[26];
+        for (char c : s1.toCharArray()) {
+            vals[c - 'a']++;
+        }
+        int counts[] = new int[26];
+        int index = 0;
+        while (index + s1.length() <= s2.length()) {
+            if (index == 0) {
+                for (int i = index; i != index + s1.length(); i++) {
+                    counts[s2.charAt(i) - 'a']++;
+                }
+            } else {
+                counts[s2.charAt(index - 1) - 'a']--;
+                counts[s2.charAt(index + s1.length() - 1) - 'a']++;
+            }
+            if (Arrays.equals(vals, counts)) {
+                return true;
+            }
+            index++;
+        }
+        return false;
+    }
+
     class Coodinate {
         int x;
         int y;
