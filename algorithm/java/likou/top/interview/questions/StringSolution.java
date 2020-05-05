f41eb83b41186f69b86289c7a101060878bf2585
zhanggang02 <zhanggang02@baidu.com>
2020-05-05 12:53:57 +0800

    longestCommonPrefix

--- algorithm/java/likou/top/interview/questions/StringSolution.java
+++ algorithm/java/likou/top/interview/questions/StringSolution.java
@@ -670,6 +670,42 @@ public class StringSolution {
         return r;
     }
 
+    /*    最长公共前缀
+        编写一个函数来查找字符串数组中的最长公共前缀。
+
+        如果不存在公共前缀，返回空字符串 ""。
+
+        示例 1:
+
+        输入: ["flower","flow","flight"]
+        输出: "fl"
+        示例 2:
+
+        输入: ["dog","racecar","car"]
+        输出: ""
+        解释: 输入不存在公共前缀。
+        说明:
+
+        所有输入只包含小写字母 a-z 。*/
+    public String longestCommonPrefix(String[] strs) {
+        if (strs.length == 0) {
+            return "";
+        }
+        String r = strs[0];
+        for (int i = 1; i != strs.length; i++) {
+            int c1 = 0, c2 = 0;
+            while (c1 != r.length() && c2 != strs[i].length() && r.charAt(c1) == strs[i].charAt(c2)) {
+                c1++;
+                c2++;
+            }
+            r = r.substring(0, c1);
+            if (r.equals("")) {
+                return r;
+            }
+        }
+        return r;
+    }
+
     class Coodinate {
         int x;
         int y;
@@ -705,4 +741,5 @@ public class StringSolution {
             return Objects.hash(x, y);
         }
     }
+
 }
