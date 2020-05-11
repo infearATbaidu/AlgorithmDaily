d87a506c35e98d502638c04b48351c372fabea63
zhanggang02 <zhanggang02@baidu.com>
2020-05-11 21:28:28 +0800

    maxAreaOfIsland

--- algorithm/java/likou/top/interview/questions/ArraySolution.java
+++ algorithm/java/likou/top/interview/questions/ArraySolution.java
@@ -375,6 +375,57 @@ solution.shuffle();*/
         return sb.toString();
     }
 
+    /*    岛屿的最大面积
+        给定一个包含了一些 0 和 1 的非空二维数组 grid 。
+        一个 岛屿 是由一些相邻的 1 (代表土地) 构成的组合，这里的「相邻」要求两个 1 必须在水平或者竖直方向上相邻。你可以假设 grid 的四个边缘都被 0（代表水）包围着。
+        找到给定的二维数组中最大的岛屿面积。(如果没有岛屿，则返回面积为 0 。)
+        示例 1:
+
+                [[0,0,1,0,0,0,0,1,0,0,0,0,0],
+                [0,0,0,0,0,0,0,1,1,1,0,0,0],
+                [0,1,1,0,1,0,0,0,0,0,0,0,0],
+                [0,1,0,0,1,1,0,0,1,0,1,0,0],
+                [0,1,0,0,1,1,0,0,1,1,1,0,0],
+                [0,0,0,0,0,0,0,0,0,0,1,0,0],
+                [0,0,0,0,0,0,0,1,1,1,0,0,0],
+                [0,0,0,0,0,0,0,1,1,0,0,0,0]]
+        对于上面这个给定矩阵应返回 6。注意答案不应该是 11 ，因为岛屿只能包含水平或垂直的四个方向的 1 。*/
+    public int maxAreaOfIsland(int[][] grid) {
+        int max = 0;
+        if (grid.length == 0 || grid[0].length == 0) {
+            return max;
+        }
+        for (int i = 0; i != grid.length; i++) {
+            for (int j = 0; j != grid[0].length; j++) {
+                if (grid[i][j] != 1) {
+                    continue;
+                }
+                int r = search(grid, i, j);
+                max = Math.max(r, max);
+            }
+        }
+        return max;
+    }
+
+    private int search(int[][] grid, int i, int j) {
+        // tag grid[i][j] as visited.
+        grid[i][j] = -1;
+        int r = 1;
+        if (i - 1 >= 0 && grid[i - 1][j] == 1) {
+            r += search(grid, i - 1, j);
+        }
+        if (i + 1 < grid.length && grid[i + 1][j] == 1) {
+            r += search(grid, i + 1, j);
+        }
+        if (j - 1 >= 0 && grid[i][j - 1] == 1) {
+            r += search(grid, i, j - 1);
+        }
+        if (j + 1 < grid[0].length && grid[i][j + 1] == 1) {
+            r += search(grid, i, j + 1);
+        }
+        return r;
+    }
+
     class Solution {
         private int origin[];
 
