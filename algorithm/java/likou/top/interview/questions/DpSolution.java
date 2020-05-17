195a54855ac26c11eb96863515c76c646fb16672
zhanggang02 <zhanggang02@baidu.com>
2020-05-17 17:40:13 +0800

    optimize 'lengthOfLIS' implementation

--- algorithm/java/likou/top/interview/questions/DpSolution.java
+++ algorithm/java/likou/top/interview/questions/DpSolution.java
@@ -14,6 +14,10 @@ import leetcode.TreeNode;
  */
 public class DpSolution {
 
+    public static void main(String args[]) {
+        new DpSolution().lengthOfLIS(new int[] {4, 10, 4, 3, 8, 9});
+    }
+
     /*    完全平方数
         给定正整数 n，找到若干个完全平方数（比如 1, 4, 9, 16, ...）使得它们的和等于 n。你需要让组成和的完全平方数的个数最少。
 
@@ -123,31 +127,16 @@ public class DpSolution {
         return max;
     }
 
-/*    最长上升子序列
-    给定一个无序的整数数组，找到其中最长上升子序列的长度。
-
-    示例:
-
-    输入: [10,9,2,5,3,7,101,18]
-    输出: 4
-    解释: 最长的上升子序列是 [2,3,7,101]，它的长度是 4。
-    说明:
-
-    可能会有多种最长上升子序列的组合，你只需要输出对应的长度即可。
-    你算法的时间复杂度应该为 O(n2) 。
-    进阶: 你能将算法的时间复杂度降低到 O(n log n) 吗?*/
-
     public int lengthOfLIS(int[] nums) {
         if (nums.length == 0) {
             return 0;
         }
         int[] result = new int[nums.length];
-        int cur = 1;
-        result[0] = 1;
-        for (int i = 1; i != nums.length; i++) {
-            int max = 1;
-            for (int j = 0; j != i; j++) {
-                if (nums[j] >= nums[i]) {
+        int cur = 0;
+        for (int i = 0; i != nums.length; i++) {
+            int max = 0;
+            for (int j = 0; j <= i; j++) {
+                if (nums[j] >= nums[i] && j != i) {
                     continue;
                 }
                 max = Math.max(max, result[j] + 1);
