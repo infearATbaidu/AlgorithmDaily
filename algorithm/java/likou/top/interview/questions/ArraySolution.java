0cbb4f43ae345278bf62d77eb503f2658782a031
zhanggang02 <zhanggang02@baidu.com>
2020-06-04 16:06:53 +0800

    add "qucik sort" implementation

--- algorithm/java/likou/top/interview/questions/ArraySolution.java
+++ algorithm/java/likou/top/interview/questions/ArraySolution.java
@@ -265,7 +265,7 @@ public class ArraySolution {
     输入: [5,4,3,2,1]
     输出: false*/
 
-    private void swap(int[] nums, int start, int end) {
+    private void swapRange(int[] nums, int start, int end) {
         int i = start, j = end - 1;
         while (i < j) {
             int tmp = nums[i];
@@ -583,6 +583,42 @@ solution.shuffle();*/
             }
         }
         return true;
+    }
+
+    /*
+        给你一个整数数组 nums，请你将该数组升序排列。
+    */
+    public int[] sortArray(int[] nums) {
+        return quickSort(nums, 0, nums.length - 1);
+    }
+
+    private int[] quickSort(int[] arr, int left, int right) {
+        if (left < right) {
+            int partitionIndex = partition(arr, left, right);
+            quickSort(arr, left, partitionIndex - 1);
+            quickSort(arr, partitionIndex + 1, right);
+        }
+        return arr;
+    }
+
+    private int partition(int[] arr, int left, int right) {
+        // 设定基准值（pivot）
+        int pivot = left;
+        int index = pivot + 1;
+        for (int i = index; i <= right; i++) {
+            if (arr[i] < arr[pivot]) {
+                swap(arr, i, index);
+                index++;
+            }
+        }
+        swap(arr, pivot, index - 1);
+        return index - 1;
+    }
+
+    private void swap(int[] arr, int i, int j) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
     }
 
     class Solution {
