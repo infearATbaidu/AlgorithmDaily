fd71969fc2331d318769be4d30cedc7fa1c889a2
zhanggang02 <zhanggang02@baidu.com>
2020-06-04 15:16:08 +0800

    add "maxSubArray" implementation

--- algorithm/java/likou/top/interview/questions/DpSolution.java
+++ algorithm/java/likou/top/interview/questions/DpSolution.java
@@ -434,4 +434,25 @@ public class DpSolution {
         return max + 1;
     }
 
+    /*    最大子序和
+        给定一个整数数组 nums ，找到一个具有最大和的连续子数组（子数组最少包含一个元素），返回其最大和。
+
+        示例:
+
+        输入: [-2,1,-3,4,-1,2,1,-5,4],
+        输出: 6
+        解释: 连续子数组 [4,-1,2,1] 的和最大，为 6。*/
+    public int maxSubArray(int[] nums) {
+        if (nums.length == 0) {
+            return 0;
+        }
+        int[] result = new int[nums.length];
+        int max = result[nums.length - 1] = nums[nums.length - 1];
+        for (int index = nums.length - 2; index != -1; index--) {
+            result[index] = Math.max(nums[index], nums[index] + result[index + 1]);
+            max = Math.max(max, result[index]);
+        }
+        return max;
+    }
+
 }
