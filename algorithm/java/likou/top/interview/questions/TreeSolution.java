4156dfa3dd84840d5f6ec759869e75edcf8a645a
zhanggang02 <zhanggang02@baidu.com>
2020-06-06 23:29:53 +0800

    add "isSubStructure" implementation

--- algorithm/java/likou/top/interview/questions/TreeSolution.java
+++ algorithm/java/likou/top/interview/questions/TreeSolution.java
@@ -11,6 +11,7 @@ import java.util.Map;
  * @author infear
  */
 public class TreeSolution {
+
     public static void main(String args[]) {
         TreeNode t1 = new TreeNode(1);
         TreeNode t2 = new TreeNode(2);
@@ -22,6 +23,47 @@ public class TreeSolution {
         TreeNode root = Codec.deserialize(ser);
         ser = Codec.serialize(root);
         System.out.println(ser);
+    }
+
+    /*    https://leetcode-cn.com/problems/shu-de-zi-jie-gou-lcof/
+        输入两棵二叉树A和B，判断B是不是A的子结构。(约定空树不是任意一个树的子结构)
+        B是A的子结构， 即A中有出现和B相同的结构和节点值。
+        例如:
+        给定的树 A:
+
+                     3
+                    / \
+                   4   5
+                  / \
+                 1   2
+        给定的树 B：
+
+                   4 
+                  /
+                 1
+        返回 true，因为 B 与 A 的一个子树拥有相同的结构和节点值。*/
+    public boolean isSubStructure(TreeNode A, TreeNode B) {
+        if (A == null || B == null) {
+            return false;
+        }
+        if (contains(A, B)) {
+            return true;
+        }
+        return isSubStructure(A.left, B) || isSubStructure(A.right, B);
+    }
+
+    private boolean contains(TreeNode a, TreeNode b) {
+        if (b == null) {
+            return true;
+        }
+        if (a == null) {
+            return false;
+        }
+        if (a.val != b.val) {
+            return false;
+        }
+        return contains(a.left, b.left) && contains(a.right, b.right);
+
     }
 
     public int kthSmallest(TreeNode root, int k) {
